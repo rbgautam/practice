d115ca2b430a1bd4907ababd3565479bf40f68e8
rbgautam <rahulbgautam@gmail.com>
2017-01-27 16:10:22 -0600

    Intit Commit for AspNetIdentity

---
+++ AspNetIdentity/AspNetIdentity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetIdentity.Startup))]
+namespace AspNetIdentity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
